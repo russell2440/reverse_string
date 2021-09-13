0b95aa8d89e42949fb1126c39ba64fe6d2fbe969
russell2440 <85845760+russell2440@users.noreply.github.com>
2021-09-13 19:35:13 +0000

    update

--- main.cpp
+++ main.cpp
@@ -4,16 +4,29 @@
 
 using namespace std;
 
-string reverse_string(string str){
+string reverse_string_iter(string str){
+  string rs=str;
+  for(unsigned int i=0, j=str.length(); i<j; i++){
+    rs[j-i-1] = str[i];
+  }
+  return rs;
+}
+
+
+string reverse_string_recur(string str){
   if (str == ""){
     return("");
   } else {
-    return(reverse_string(str.substr(1)) + str.at(0));
+    return(reverse_string_recur(str.substr(1)) + str.at(0));
   }
 }
+
+
 int main() {
   string s = "Hello World!";
   cout<<"s: "<<s<<endl;
-  string r = reverse_string(s);
+  string r = reverse_string_iter(s);
   cout<<"r: "<<r<<endl;
+  string rc = reverse_string_recur(s);
+  cout<<"rc: "<<rc<<endl;
 }
